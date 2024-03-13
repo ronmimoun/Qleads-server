bd1fee5713663d22f00b16057551911b45c4455e
ronmimoun <ron@mobilitier.com>
2024-03-13 22:35:30 +0200

    Changing database url

--- services/db.service.js
+++ services/db.service.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose")
 const dotenv = require("dotenv");
 dotenv.config();
 
-const MONGO_URL = process.env.NODE_ENV !== "production" ? process.env.MONGO_URL : 'mongodb://localhost:27017/qleads'
+// const MONGO_URL = process.env.NODE_ENV !== "production" ? process.env.MONGO_URL : 'mongodb://localhost:27017/qleads'
+const MONGO_URL = process.env.NODE_ENV !== "production" ? process.env.MONGO_URL : process.env.MONGO_URL_PRODUCTION
 
 mongoose.connect(MONGO_URL).then(() => {
     console.log("Connected to DB");
