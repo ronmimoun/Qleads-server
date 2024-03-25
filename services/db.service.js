4f8380ef4d03ff1018a3ec0517bed90642e30e7a
ronmimoun <ron@mobilitier.com>
2024-03-25 20:55:19 +0200

    Changing condition

--- services/db.service.js
+++ services/db.service.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 const dotenv = require("dotenv");
 dotenv.config();
 
-const MONGO_URL = process.env.NODE_ENV !== "production" ? process.env.MONGO_URL : process.env.MONGO_URL_PRODUCTION
+const MONGO_URL = process.env.NODE_ENV === "production" ? process.env.MONGO_URL_PRODUCTION : process.env.MONGO_URL
 
 mongoose.connect(MONGO_URL).then(() => {
     console.log("Connected to DB");
